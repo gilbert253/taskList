9a3df76d4f9c4f8ff1e2f029836ac5ea17cbd93f
gilbert253 <167355373+gilbert253@users.noreply.github.com>
2024-06-11 20:12:05 -0700

    Update taskList.js

--- taskList.js
+++ taskList.js
@@ -4,9 +4,7 @@ const listDiv = document.getElementById("list-div")
 const input = document.getElementById("input")
 const btn = document.getElementById("button")
 const radio = document.getElementById("radio")
-const saveFunction = () => {
-    localStorage.setItem("data", listDiv.innerHTML)
-    }
+
     
 
 const addTask = () => {
@@ -39,8 +37,23 @@ listDiv.addEventListener("click", (e) => {
     } 
 },false)
 
-
+const saveFunction = () => {
+    localStorage.setItem("data", listDiv.innerHTML)
+    }
 const showData = () => {
 listDiv.innerHTML = localStorage.getItem("data")
 }
 showData()
+
+/*
+need to have a growing list of tasks
+ability to click x to delete
+ability to click check and cross it out
+date field would be good
+
+
+if input is submitted a task with that text is created
+input text should be saved on local storage
+if check radio is clicked then strikethrougho on text
+if x is clicked then task is deleted
+*/
